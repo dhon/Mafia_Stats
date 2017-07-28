bd54481e46bd4da7e7b867d8757e7ebc7bfadac7
dhon <morgangrimes14@gmail.com>
2017-07-28 04:51:43 -0500

    Added week 7/26

--- js/services.js
+++ js/services.js
@@ -20,7 +20,7 @@ angular.module('app.services', [])
                 data[i] = json;
             })(); 
         }
-        for(var i = 9; i < 58; i++){
+        for(var i = 9; i < 60; i++){
             var json = (function () {
                 var json = null;
                 $.ajax({
