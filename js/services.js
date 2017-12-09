eac3a03ceac40b3312a45b416f45df5fbfc9bdcf
dhon <morgangrimes14@gmail.com>
2017-12-08 18:22:46 -0600

    Add week 12/06

--- js/services.js
+++ js/services.js
@@ -5,7 +5,7 @@ angular.module('app.services', [])
     var data = [];
 
     function getJSON(){
-        for(var i = 0; i < 94; i++){
+        for(var i = 0; i < 96; i++){
             var ourRequest = new XMLHttpRequest();
             var num = i < 9 ? '0' + (i+1) : (i+1);
             var URL = 'http://localhost:3000/' + num + '.json'
